3f786273b42f428fc75bde10ad3a3a8c9e5998a4
Rachel Allen <rallenco13@gmail.com>
2022-04-12 18:00:31 -0600

    created idea class

---
+++ Idea.js
@@ -0,0 +1,17 @@
+
+// saveToStorage* - only used for extensions (should only have one job which is to save the instance to storage)
+// deleteFromStorage* - only used for extensions
+
+ class Idea() {
+   constructor(title, body) {
+     this.id = Date.now();
+     this.title = title || ""
+     this.body = body || ""
+     this.starred = false
+   }
+
+   updateIdea() {
+  // updateIdea (should update the idea’s starred state
+    this.starred = !this.starred
+   }
+ }
--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+// A main.js file that contains all DOM related JavaScript.
+// query selector variables go here 👇
 var searchButton = document.querySelector(".search-button");
+
+// we've provided you with some data to work with 👇
+var savedIdeas = [];
+
+// event listeners go here 👇
+
+// functions and event handlers go here 👇
