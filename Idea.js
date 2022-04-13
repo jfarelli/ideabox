43fa778fc9ae4fcbbdf8802d945b3c6729de960e
Jordan Farelli <jordanfarelli@gmail.com>
2022-04-12 19:33:25 -0600

    Created functionality w/ text input delete on save and disabling the save button when no text has been inputed

---
+++ Idea.js
@@ -0,0 +1,12 @@
+class Idea {
+  constructor() {
+    this.id = Date.now();
+    this.title = [];
+    this.body = [];
+    this.star = false;
+  }
+
+  updateIdea() {
+// function updates the idea's starred state
+  }
+}
--- main.js
+++ main.js
@@ -1,1 +1,48 @@
-var searchButton = document.querySelector(".search-button");
+// querySelector
+// var searchButton = document.querySelector(".search-button");
+var ideaTitle = document.getElementById("idea-title");
+var ideaBody = document.getElementById("body-title");
+// var clearTitle = document.querySelector("#idea-title");
+// var clearBody = document.querySelector("#body-title");
+var saveIdeaButton = document.getElementById("save-button");
+
+
+
+
+// delete input-form after hitting save button
+var formInput = document.getElementById('idea-form');
+
+formInput.addEventListener('submit', function handleSubmit(event) {
+  event.preventDefault();
+  formInput.reset();
+});
+
+
+// disable save button (w/ lighter color & no pointer) when no input in Title or Body
+ideaTitle.addEventListener('keyup', handleKeyup);
+ideaBody.addEventListener('keyup', handleKeyup);
+
+function handleKeyup(event) {
+  if (ideaTitle.value && ideaBody.value){
+    saveIdeaButton.disabled = false;
+  } else {
+    saveIdeaButton.disabled = true;
+  }
+}
+
+
+
+
+
+
+
+
+// still working on the saveIdea function
+// function saveIdea(title, body) {
+//   event.preventDefault();
+//   var savedIdeaCard = new Idea(title, body);
+//   title.push(savedIdeaCard.title);
+//   body.push(savedIdeaCard.body);
+//
+//   return savedIdeaCard;
+// }
